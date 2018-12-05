95e4d6fbbae8396c9cf9a0313ca39aefba93cf57
jaime veliz <jricardovm@gmail.com>
2018-12-05 08:38:52 -0400

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'paises-angular-lab';
+
+private paises = null;
+constructor(private http: HttpClient) {}
+
+ngOnInit(){
+  this.http.get("https://restcountries.eu/rest/v2/all")
+  .subscribe(
+    result => {
+      this.paises = result;
+    },
+    error => {
+      console.log('error consumer https://restcountries.eu/rest/v2/all');
+    }
+  );
+}
+
+
+}
